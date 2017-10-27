4282d1f7af49b2f0d5a9450255085c12ec1ff126
santicalle <santiago.calle@hotmail.com>
2017-10-27 00:57:26 -0500

    cambios server

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var history = require('connect-history-api-fallback');
-
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express()
-//add this middleware
-app.use(history());    
-app.use(serveStatic(__dirname))
+// server.js
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 8000
 app.listen(port, function() {
   console.log("App is running on port " + port);
 });
+//console.log('server started '+ port);
