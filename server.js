08863b8bdba5a2a8e646d538025610ceb20af91e
santicalle <santiago.calle@hotmail.com>
2017-10-27 00:38:39 -0500

    cambios server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app = express()
 //add this middleware
 app.use(history());    
 app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started '+ port) 
+var port = process.env.PORT || 8000
+app.listen(port, function() {
+  console.log("App is running on port " + port);
+});
