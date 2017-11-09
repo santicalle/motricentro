812775ef66b1ccddc1830d0777ce900773edc730
santicalle <santiago.calle@hotmail.com>
2017-11-09 14:19:38 -0500

    prueba deploy en heroku

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -9,7 +9,7 @@
       <div class="col-md-12 col-sm-12">
         <div class="card">
           <div class="card-header">
-            <h5>Notificaciones</h5>
+            <h5>Notificacionessss</h5>
           </div>
           <div class="card-block panels-wells">
             <!--<div class="alert alert-info background-info">
